30600946e7095fc9a4896255831af7126007d02b
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2026-05-21 02:00:09 +0600

    Define reusable function for responses

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express"
 import { authService } from "./auth.service"
+import { sendResponse } from "../utility/sendResponse"
 
 const { addUserToDB, verifyUserInDB } = authService
 
@@ -7,10 +8,10 @@ const addUser = async (req: Request, res: Response) => {
 
     try {
         const result = await addUserToDB(req.body)
-        res.status(201).send({ success: true, message: 'User registered successfully', data: result })
+        sendResponse(res, { status_code: 201, success: true, message: 'User registered successfully', data: result })
     }
-    catch (error) {
-        res.status(500).send({ success: false, error: error })
+    catch (error: any) {
+        sendResponse(res, { status_code: 500, success: false, error: error.message })
     }
 }
 
@@ -18,10 +19,10 @@ const verifyUser = async (req: Request, res: Response) => {
 
     try {
         const result = await verifyUserInDB(req.body)
-        res.status(201).send({ success: true, message: 'Login successful', data: result })
+        sendResponse(res, { status_code: 200, success: true, message: 'Login successful', data: result })
     }
     catch (error: any) {
-        res.status(500).send({ success: false, error: error.message })
+        sendResponse(res, { status_code: 500, success: false, error: error.message })
     }
 }
 
---
+++ src/modules/types/responseInfo.ts
@@ -0,0 +1,7 @@
+export interface ResponseInfo<X, Y> {
+    status_code: number,
+    success: boolean,
+    message?: string,
+    data?: X,
+    error?: Y
+}
